1784784656fb8433b8e5fe1b0a635d215e2adb18
muhammadqodir.a <azamovm323@gmail.com>
2024-08-18 15:21:05 +0500

    _5_Longest_Palindromic_Substring

---
+++ Leetcode/5_Longest_Palindromic_Substring.cs
@@ -0,0 +1,44 @@
+﻿namespace Leetcode
+{
+    public class _5_Longest_Palindromic_Substring
+    {
+        public string LongestPalindrome(string s)
+        {
+            try
+            {
+                if (s.Length == 1) return s;
+                string maxWord = "";
+                int FirstIndeks = 0;
+
+                string temp = new string(s.Reverse().ToArray());
+                if (temp.Equals(s)) return s;
+
+                for (int i = 0; i < s.Length; i++)
+                {
+                    FirstIndeks = s.IndexOf(s[i], FirstIndeks + 1);
+
+                    if (FirstIndeks >= 0)
+                    {
+                        string helper = s.Substring(i, FirstIndeks - i + 1);
+
+                        string str = new string(helper.Reverse().ToArray());
+                        if (helper.Equals(str) && helper.Length > maxWord.Length)
+                            maxWord = helper;
+                        i--;
+                    }
+                    else
+                        FirstIndeks = i;
+
+                }
+
+                return maxWord == "" ? s[0].ToString() : maxWord;
+            }
+            catch (Exception)
+            {
+
+                throw ;
+            }
+            
+        }
+    }
+}
--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -3,9 +3,10 @@
     internal class Program
     {
         static _3_Longest_Substring_Without_Repeating_Characters solution_3 = new();
+        static _5_Longest_Palindromic_Substring solution_5 = new();
         static void Main(string[] args)
         {
-            Console.WriteLine(solution_3.LengthOfLongestSubstring("pwwkew"));
+            Console.WriteLine(solution_5.LongestPalindrome("aacabdkacaa"));
         }
     }
 }
