0e81b19a006661845c7042e65e5d32fa9af963c8
muhammadqodir.a <azamovm323@gmail.com>
2024-08-18 14:07:02 +0500

    _3_Longest_Substring_Without_Repeating_Characters

---
+++ Leetcode/3_Longest_Substring_Without_Repeating_Characters.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public  class _3_Longest_Substring_Without_Repeating_Characters
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            try
+            {
+                int[] arr = new int[255];
+                int currentLeng = 0;
+                int firstIndex = 0;
+                int maxLength = 0;
+                for (int i = 0; i < s.Length; i++)
+                {
+                    int n = s[i];
+                    arr[n]++;
+                    if (arr[n] > 1)
+                    {
+                        arr[n]--;
+                        if (s.IndexOf(s[i], firstIndex) != i)
+                        {
+                            if (maxLength == 0)
+                            {
+                                maxLength = i;
+                            }
+
+                            if (maxLength < i - firstIndex)
+                            {
+                                maxLength = i - firstIndex;
+
+                            }
+                            firstIndex = s.IndexOf(s[i], firstIndex) + 1;
+                        }
+                        currentLeng = i - firstIndex + 1;
+                    }
+                    else
+                    {
+                        currentLeng++;
+                    }
+                    
+                    if (maxLength < currentLeng)
+                    {
+                        maxLength = currentLeng;
+                    }
+                }
+                return maxLength;
+            }
+            catch { return 0; }
+        }
+    }
+}
---
+++ Leetcode/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Leetcode
+{
+    internal class Program
+    {
+        static _3_Longest_Substring_Without_Repeating_Characters solution_3 = new();
+        static void Main(string[] args)
+        {
+            Console.WriteLine(solution_3.LengthOfLongestSubstring("pwwkew"));
+        }
+    }
+}
